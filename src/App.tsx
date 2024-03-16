ec964361826a5f40e4d360eeaf48ef9af6ec943e
Jesus Castaneda <imfanjs@gmail.com>
2024-03-16 10:39:49 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>Welcome to your Interview Exercise</p>
+        <p>
+          Edit <code>src/App.tsx</code> and start building your app
+        </p>
+        <p>
+          Create any component you need and use good practices and clean code.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
